43bccc0139f867e10765b40883cce6ff8bb31861
last night <fujizhang2525@gmail.com>
2021-10-13 00:56:52 -0400

    init

---
+++ src/Search.jsx
@@ -0,0 +1,13 @@
+
+function Search() {
+    return (
+      <div className="search">
+       <input></input>
+       <div className="search_"
+      </div>
+    );
+}
+  
+
+
+export default Search;
